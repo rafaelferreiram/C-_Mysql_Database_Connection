38eac0ce36460e4d8eef3f154b1fbd1ccbce4345
Rafael Ferreira <rafah.26ferreira@hotmail.com>
2018-03-02 10:28:41 -0300

    Firs Commit

---
+++ Teste/Infra/Factory.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste.Infra
+{
+    public static class Factory
+    {
+        public static MySqlConnection getConnection()
+        {
+            string strConexao = ConfigurationManager.ConnectionStrings["projetoBanco"].ConnectionString;
+            MySqlConnection cnn = new MySqlConnection(strConexao);
+            cnn.Open();
+
+            return cnn;
+
+        }
+    }
+}
---
+++ Teste/Program.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teste.Infra;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MySqlConnection cnn = Factory.getConnection();
+            try
+            {
+
+                MySqlCommand comando = cnn.CreateCommand();
+                comando.CommandText = "INSERT INTO databse values(, );";
+                comando.ExecuteNonQuery();
+                Console.WriteLine("Conexão feita e dado cadastrado com sucesso");
+                Console.ReadLine();
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                       Console.WriteLine("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }            
+         }
+      }
+   }
+}
